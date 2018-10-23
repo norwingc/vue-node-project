2058decc758fcc14c211d7b505791e1344ff994b
Norwin Guerreo <norwingcruz@gmail.com>
2018-10-22 21:04:28 -0500

    setting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h1>Register</h1>
+		<input type="email" name="emial" placeholder="email" v-model="email"> <br>
+		<input type="password" name="password" placeholder="password" v-model="password"> <br>
+		<button @click="register">Register</button>
+	</div>
+</template>
+
+<script>
+
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+	data(){
+		return{
+			email: '',
+			password: ''
+		}
+	},
+	methods:{
+		async register(){
+			const resp = await AuthenticationService.register({
+				email: this.email,
+				password: this.password
+			})
+
+			console.log(resp.data)
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,16 @@
-const express   = require('express')
+const express    = require('express')
 const bodyParser = require('body-parser')
-const cors      = require('cors')
-const morgan    = require('morgan')
+const cors       = require('cors')
+const morgan     = require('morgan')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
 	res.send({
-		messsage: 'hello word'
+		messsage: `Hello ${req.body.email}!`
 	});
 });
 
