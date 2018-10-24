2552a7c812c22a1871f08f2cc55dd187f552f595
norwingc <norwingcruz@gmail.com>
2018-10-24 16:52:39 -0500

    update DB dialect to Mysql

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,12 +2,12 @@ module.exports = {
 	port: process.env.PORT || 8081,
 	db: {
 		database: process.env.DB_NAME || 'tabtracker',
-		user: process.env.DB_USER || 'tabtracker',
-		password: process.env.DB_PASS || 'tabtracker',
+		user: process.env.DB_USER || 'root',
+		password: process.env.DB_PASS || '',
 		options: {
-			dialect: process.env.DIALECT || 'sqlite',
+			dialect: process.env.DIALECT || 'mysql',
 			host: process.env.HOST || 'localhost',
-			storage: './tabtracker.sqlite'
+			port: 3306
 		}
 	}
 }
