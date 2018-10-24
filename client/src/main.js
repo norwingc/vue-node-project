14d8249e042d2142f812acb230cccd6687308961
norwingc <norwingcruz@gmail.com>
2018-10-24 14:30:59 -0500

    connecting to sqlite using sequelize and freshening up the register page

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuetify, {
+	iconfont: 'mdi'
+})
 /* eslint-disable no-new */
 new Vue({
 	el: '#app',
