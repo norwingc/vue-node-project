bb9455488da1c4db9404cf73e328fd9368086350
norwingc <norwingcruz@gmail.com>
2018-11-27 11:08:51 -0500

    update

--- client/src/components/Song.vue
+++ client/src/components/Song.vue
@@ -1,18 +1,31 @@
 <template>
 	<v-layout>
 		<v-flex>
-			<panel title="Songs"></panel>
+			<panel title="Songs">
+				<div v-for="song in songs" :key="song.title">
+
+				</div>
+			</panel>
 		</v-flex>
 	</v-layout>
 </template>
 
 <script>
 
+import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 
 export default {
 	components: {
 		Panel
+	},
+	data(){
+		return{
+			songs: null
+		}
+	},
+	mounted(){
+		this.songs = SongsService.index()
 	}
 }
 </script>
